77159c23dda49a2b26c5b1af7405744435808241
Tinghhh <605358999@qq.com>
2024-09-02 17:10:21 +0800

    1

---
+++ src/settings.b13be.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d17e2",resources:"74de5",main:"20176"}};
